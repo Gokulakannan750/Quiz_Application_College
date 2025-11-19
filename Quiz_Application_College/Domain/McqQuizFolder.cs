bcf2d0384bec851311fb3256fe2fb44e90ee941d
Gokulakannan750 <gokulakannankrish51@gmail.com>
2025-11-19 15:37:58 +0530

    Working MCQ quizzes Folder

---
+++ Quiz_Application_College/Domain/McqQuizFolder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quiz_Application_College.Domain
+{
+    public class McqQuizFolder
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = default!;
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        // For sorting folders in the list
+        public int OrderNo { get; set; }
+
+        // All MCQ quizzes that belong to this folder
+        public ICollection<Quiz> Quizzes { get; set; } = new List<Quiz>();
+    }
+}
